57b583ed140df7024bcae2066c2032e47f9b32cc
Wyero JR <kajuwyero@gmail.com>
2022-08-09 00:05:28 +0700

    test

--- slicing html/js/main.js
+++ slicing html/js/main.js
@@ -4,3 +4,37 @@ const nav = document.querySelector("nav ul")
 toggle.addEventListener("click", () => {
     nav.classList.toggle("slide")
 })
+
+// search
+const searching = document.querySelector(".searching")
+const input = document.querySelector(".input")
+
+searching.addEventListener("click", ()=>{
+    if(!input.classList.contains("search-open")){
+        input.classList.add("search-open")
+    }else{
+        input.classList.remove('search-open')
+    }
+})
+
+//slider
+
+let slideIndex = 1;
+        showContent(slideIndex)
+        function btn_slide(n){
+            showContent(slideIndex += n)
+        }
+        function showContent(n){
+            let i
+            const content = document.getElementsByClassName('content')
+            if(n>content.length){
+                slideIndex = 1
+            }
+            if(n<1){
+                slideIndex = content.length
+            }
+            for(i=0; i<content.length; i++){
+                content[i].style.display="none"
+            }
+            content[slideIndex-1].style.display="block"
+        }
